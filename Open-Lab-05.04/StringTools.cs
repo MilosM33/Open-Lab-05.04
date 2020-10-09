18352b19c08a8c3119b96a72e64d6f7f3fadec89
MilosM33 <milosmarejka89@gmail.com>
2020-10-09 21:10:24 +0200

    init

--- Open-Lab-05.04/StringTools.cs
+++ Open-Lab-05.04/StringTools.cs
@@ -1,12 +1,10 @@
 ﻿using System;
-
+using System.Text.RegularExpressions;
 namespace Open_Lab_05._04
 {
     public class StringTools
     {
-        public string RemoveSpecialCharacters(string original)
-        {
-            throw new NotImplementedException();
-        }
+        public string RemoveSpecialCharacters(string original) => Regex.Replace(original, "[^a-zA-Z-0-9 _-]", "");
+        
     }
 }
